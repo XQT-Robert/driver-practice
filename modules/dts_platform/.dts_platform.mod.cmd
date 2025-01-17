7f9109fffebef640b625e9c5b1a2ba50b2def44c
RobertXu <xqt_robert@126.com>
2025-01-18 01:04:30 +0800

    regular

---
+++ modules/dts_platform/.dts_platform.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/robert/share_doc/MyShare/driver_practice/modules/dts_platform/dts_platform.mod := { echo  /home/robert/share_doc/MyShare/driver_practice/modules/dts_platform/dts_platform.o; echo; } > /home/robert/share_doc/MyShare/driver_practice/modules/dts_platform/dts_platform.mod
---
+++ modules/dts_platform/dts_platform.mod
@@ -0,0 +1,2 @@
+/home/robert/share_doc/MyShare/driver_practice/modules/dts_platform/dts_platform.o
+
